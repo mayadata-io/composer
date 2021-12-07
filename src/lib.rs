86e5a1ac4942662f94b933f1bef6dbadf53d9e48
Mikhail Tcymbaliuk <mtzaurus@gmail.com>
2021-12-07 16:27:48 +0100

    feat(composer): Composer library needs to be a separate crate
    Composer library is now a separate crate which simplifies reuse
    for all tests that require high-level Docker container management.
    
    Resolves: CAS-1255

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod composer;
+pub mod rpc;
+
+pub use composer::{Binary, Builder, BuilderConfigure, ComposeTest, ContainerSpec, RpcHandle};
---
+++ src/rpc.rs
@@ -0,0 +1,3 @@
+pub mod mayastor {
+    include!(concat!(env!("OUT_DIR"), "/mayastor.rs"));
+}
